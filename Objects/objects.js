e556d0149b0bc9f0641c5e1ce4cfe78efd56cc81
Phuong Nguyen <phuongsover2@gmail.com>
2022-12-10 00:01:34 +0700

    Excess properties.

--- Objects/objects.js
+++ Objects/objects.js
@@ -1,15 +1,25 @@
 "use strict";
 // A function with an object type parameter
-// const printName = (name: { first: string; last: string }): void => {
-//   console.log(`Name: ${name.first} ${name.last}`);
-// };
+const printName = (name) => {
+    console.log(`Name: ${name.first} ${name.last}`);
+};
+const singer = {
+    first: 'Phuong',
+    last: 'Nguyen',
+    age: 22,
+    isAlive: true,
+};
+printName(singer);
 // printName({ first: 'Phuong', last: 'Nguyen' });
-let coordinate = { x: 34, y: 100 };
+// let coordinate: { x: number; y: number } = { x: 34, y: 100 };
 const randomCoordinate = () => {
-    return {
-        x: Number(Math.random().toFixed(4)) * 100,
-        y: Number(Math.random().toFixed(4)) * 100,
-    };
+    const coordinate = { x: 1, y: 1, z: 1 };
+    return coordinate;
+    // return {
+    //   x: Number(Math.random().toFixed(4)) * 100,
+    //   y: Number(Math.random().toFixed(4)) * 100,
+    //   z: 1,
+    // };
 };
 console.log(randomCoordinate());
 console.log(randomCoordinate());
