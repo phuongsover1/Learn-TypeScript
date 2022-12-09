95cb03d4ebfe0c1ea4409764c58a6d04b22280da
Phuong Nguyen <phuongsover2@gmail.com>
2022-12-09 21:24:50 +0700

    Return Type Annotations

--- Functions/functions.js
+++ Functions/functions.js
@@ -14,8 +14,22 @@
 // ): void => {};
 // doSomething('Phuong', 21, true);
 // Working with default parameters
-const greet = (person = 'stranger') => {
-    return `Hi there, ${person}`;
-};
-console.log(greet());
-console.log(greet('Phuong'));
+// const greet = (person: string = 'stranger'): string => {
+//   return `Hi there, ${person}`;
+// };
+// console.log(greet());
+// console.log(greet('Phuong'));
+// function a(num: number): number {
+//   return 1;
+// }
+// Return TypeAnnotations
+function square(num) {
+    return num * num;
+}
+function rando(num) {
+    if (Math.random() < 0.5)
+        return num.toString();
+    return num;
+}
+console.log(rando(1));
+console.log(rando(3));
--- Functions/functions.ts
+++ Functions/functions.ts
@@ -18,9 +18,26 @@
 // doSomething('Phuong', 21, true);
 
 // Working with default parameters
-const greet = (person: string = 'stranger'): string => {
-  return `Hi there, ${person}`;
-};
+// const greet = (person: string = 'stranger'): string => {
+//   return `Hi there, ${person}`;
+// };
 
-console.log(greet());
-console.log(greet('Phuong'));
+// console.log(greet());
+// console.log(greet('Phuong'));
+
+// function a(num: number): number {
+//   return 1;
+// }
+
+// Return TypeAnnotations
+function square(num: number): number {
+  return num * num;
+}
+
+function rando(num: number): string | number {
+  if (Math.random() < 0.5) return num.toString();
+  return num;
+}
+
+console.log(rando(1));
+console.log(rando(3));
