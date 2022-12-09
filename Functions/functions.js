5100a45e89b46ebb0d7e2faf94a31573b6597b05
Phuong Nguyen <phuongsover2@gmail.com>
2022-12-09 20:51:29 +0700

    Function Parameter Annotations.

---
+++ Functions/functions.js
@@ -0,0 +1,9 @@
+"use strict";
+function square(num) {
+    return num * num;
+}
+function greet(person) {
+    return `Hi there, ${person}`;
+}
+square(3);
+greet('Phuong');
---
+++ Functions/functions.ts
@@ -0,0 +1,10 @@
+function square(num: number): number {
+  return num * num;
+}
+
+function greet(person: string): string {
+  return `Hi there, ${person}`;
+}
+
+square(3);
+greet('Phuong');
