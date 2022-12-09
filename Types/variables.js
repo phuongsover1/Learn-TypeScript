0bed93b24c2d36d3a18e367c6e0aa61b650facc5
Phuong Nguyen <phuongsover2@gmail.com>
2022-12-09 20:26:54 +0700

    Delayed Initialization & Implicit Any

--- Types/variables.js
+++ Types/variables.js
@@ -1,15 +1,33 @@
 "use strict";
-let movieTitle = 'Amadeus';
-movieTitle = 'Arrival';
-// movieTitle = 9;
-movieTitle.toUpperCase();
-let numCatLives = 9;
-numCatLives += 1;
-// numCatLives = 'zero';
-let gameOver = false;
-gameOver = true;
-// gameOver = 'true';
-null;
-undefined;
-let nothing = null;
-let foo = undefined;
+// let movieTitle: string = 'Amadeus';
+// movieTitle = 'Arrival';
+// // movieTitle = 9;
+// movieTitle.toUpperCase();
+// let numCatLives: number = 9;
+// numCatLives += 1;
+// // numCatLives = 'zero';
+// let gameOver: boolean = false;
+// gameOver = true;
+// // gameOver = 'true';
+// null;
+// undefined;
+// let nothing: null = null;
+// let foo: undefined = undefined;
+// // Type Inference
+// let number = 343;
+// number = 'dsf'; // cannot assign type 'string' to type 'number'
+// number = true; // cannot assign type 'boolean' to 'number'
+// // the any type => turn off ts complain.
+// let thing: any = 'hello';
+// thing = 1;
+// thing = false;
+// thing();
+// thing.toUpperCase();
+// Delayed Initialization & Implicit Any
+const movies = ['Arrival', 'The Thing', 'Aliens', 'Amadeus'];
+let foundMovie; // foundMovie is 'any' type
+for (let movie in movies) {
+    if (movie === 'Amadeus') {
+        foundMovie = 'Amadeus';
+    }
+}
