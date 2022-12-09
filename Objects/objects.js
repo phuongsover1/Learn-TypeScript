58728ab8fa5ebb81b0267360152e74afeb71506c
Phuong Nguyen <phuongsover2@gmail.com>
2022-12-09 23:45:45 +0700

    Object type , Object type return in typescript.

--- Objects/objects.js
+++ Objects/objects.js
@@ -1,6 +1,24 @@
 "use strict";
 // A function with an object type parameter
-const printName = (name) => {
-    console.log(`Name: ${name.first} ${name.last}`);
+// const printName = (name: { first: string; last: string }): void => {
+//   console.log(`Name: ${name.first} ${name.last}`);
+// };
+// printName({ first: 'Phuong', last: 'Nguyen' });
+let coordinate = { x: 34, y: 100 };
+const randomCoordinate = () => {
+    return {
+        x: Number(Math.random().toFixed(4)) * 100,
+        y: Number(Math.random().toFixed(4)) * 100,
+    };
 };
-printName({ first: 'Phuong', last: 'Nguyen' });
+console.log(randomCoordinate());
+console.log(randomCoordinate());
+console.log(randomCoordinate());
+console.log(randomCoordinate());
+console.log(randomCoordinate());
+console.log(randomCoordinate());
+console.log(randomCoordinate());
+console.log(randomCoordinate());
+console.log(randomCoordinate());
+console.log(randomCoordinate());
+console.log(randomCoordinate());
--- Objects/objects.ts
+++ Objects/objects.ts
@@ -1,6 +1,26 @@
 // A function with an object type parameter
-const printName = (name: { first: string; last: string }): void => {
-  console.log(`Name: ${name.first} ${name.last}`);
-};
+// const printName = (name: { first: string; last: string }): void => {
+//   console.log(`Name: ${name.first} ${name.last}`);
+// };
 
-printName({ first: 'Phuong', last: 'Nguyen' });
+// printName({ first: 'Phuong', last: 'Nguyen' });
+
+let coordinate: { x: number; y: number } = { x: 34, y: 100 };
+
+const randomCoordinate = (): { x: number; y: number } => {
+  return {
+    x: Number(Math.random().toFixed(4)) * 100,
+    y: Number(Math.random().toFixed(4)) * 100,
+  };
+};
+console.log(randomCoordinate());
+console.log(randomCoordinate());
+console.log(randomCoordinate());
+console.log(randomCoordinate());
+console.log(randomCoordinate());
+console.log(randomCoordinate());
+console.log(randomCoordinate());
+console.log(randomCoordinate());
+console.log(randomCoordinate());
+console.log(randomCoordinate());
+console.log(randomCoordinate());
