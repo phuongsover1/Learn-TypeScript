99e9c3f0632d743bf03660d2fa8f32c869a1c041
Phuong Nguyen <phuongsover2@gmail.com>
2022-12-10 16:36:35 +0700

    Complete objectsExercise

---
+++ Objects/objectsExerciseSOLUTIONS.js
@@ -0,0 +1,30 @@
+"use strict";
+const dune = {
+    title: 'Dune',
+    originalTitle: 'Dune Part One',
+    director: 'Denis Villeneuve',
+    releaseYear: 2021,
+    boxOffice: {
+        budget: 165000000,
+        grossUS: 108327830,
+        grossWorldwide: 400671789,
+    },
+};
+const cats = {
+    title: 'Cats',
+    director: 'Tom Hooper',
+    releaseYear: 2019,
+    boxOffice: {
+        budget: 95000000,
+        grossUS: 27166770,
+        grossWorldwide: 73833348,
+    },
+};
+// Write a function called getProfit that accepts a single Movie object
+// It should return the movie's worldwide gross minus its budget
+// For example...
+// getProfit(cats) => -21166652
+const getProfit = ({ boxOffice: { budget: bg, grossWorldwide: gw }, }) => {
+    return gw - bg;
+};
+console.log(getProfit(cats));
