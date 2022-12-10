0e6bb3995e971ed9f0eba7ed56bb2d95e5756114
Phuong Nguyen <phuongsover2@gmail.com>
2022-12-10 22:15:21 +0700

    Optional and readonly properties  in interfaces and types

--- 7.Interfaces/interfaces.js
+++ 7.Interfaces/interfaces.js
@@ -3,3 +3,28 @@ const loc = {
     x: 1,
     y: 2,
 };
+const Nick = {
+    name: 'Nick',
+    age: 18,
+    sayHi() {
+        console.log(`Hi, my name is ${this.name}`);
+    },
+};
+const Phuong = {
+    name: 'Phuong',
+    age: 22,
+    nickname: 'Cuong',
+    sayHi() {
+        console.log(`Hi, my name is ${this.name}`);
+    },
+};
+const temp = {
+    name: 'Phuong',
+    say() {
+        console.log(`Hi my name is ${this.name}`);
+    },
+};
+Nick.sayHi();
+Phuong.sayHi();
+temp.name;
+temp.say();
--- 7.Interfaces/interfaces.ts
+++ 7.Interfaces/interfaces.ts
@@ -8,3 +8,45 @@ const loc: PointInterface = {
   x: 1,
   y: 2,
 };
+
+// Optional and read-only properties in interfaces.
+interface Person {
+  readonly name: string;
+  age: number;
+  nickname?: string;
+  // sayHi: () => void;
+  sayHi(): void;
+}
+
+const Nick: Person = {
+  name: 'Nick',
+  age: 18,
+  sayHi(): void {
+    console.log(`Hi, my name is ${this.name}`);
+  },
+};
+
+const Phuong: Person = {
+  name: 'Phuong',
+  age: 22,
+  nickname: 'Cuong',
+  sayHi(): void {
+    console.log(`Hi, my name is ${this.name}`);
+  },
+};
+
+type Test = {
+  say: () => void;
+  name: string;
+};
+
+const temp: Test = {
+  name: 'Phuong',
+  say(): void {
+    console.log(`Hi my name is ${this.name}`);
+  },
+};
+Nick.sayHi();
+Phuong.sayHi();
+temp.name;
+temp.say();
