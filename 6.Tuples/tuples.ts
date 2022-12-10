a2f0965149b7569e21c04f033e59c8a6f7a75842
Phuong Nguyen <phuongsover2@gmail.com>
2022-12-10 20:32:47 +0700

    Tuples on TS.

---
+++ 6.Tuples/tuples.ts
@@ -0,0 +1,6 @@
+let myTuple: [number, string];
+// myTuple = [1, 'phuong', 'df']; the tuple has fixed-size
+// myTuple = ['phuong', 1]; // the type order is wrong
+myTuple = [1, 'phuong'];
+
+let numbers: [number, number, number] = [1, 2, 3];
