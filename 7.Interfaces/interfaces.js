525f8cbd9e5f66a30db3b50bd5916f7c986cdf47
Phuong Nguyen <phuongsover2@gmail.com>
2022-12-10 21:49:57 +0700

    Introducing to interfaces in TS.

---
+++ 7.Interfaces/interfaces.js
@@ -0,0 +1,5 @@
+"use strict";
+const loc = {
+    x: 1,
+    y: 2,
+};
---
+++ 7.Interfaces/interfaces.ts
@@ -0,0 +1,10 @@
+// Introducing to interfaces
+interface PointInterface {
+  x: number;
+  y: number;
+}
+
+const loc: PointInterface = {
+  x: 1,
+  y: 2,
+};
