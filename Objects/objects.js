d880135b29f247158d68fa00d83d761cf98a4904
Phuong Nguyen <phuongsover2@gmail.com>
2022-12-10 15:18:30 +0700

    Nested objects in typescript.

--- Objects/objects.js
+++ Objects/objects.js
@@ -9,28 +9,20 @@
 //   age: 22,
 //   isAlive: true,
 // };
-const randomCoordinate = () => {
-    const coordinate = { x: 1, y: 1, z: 1 };
-    return coordinate;
-    // return {
-    //   x: Number(Math.random().toFixed(4)) * 100,
-    //   y: Number(Math.random().toFixed(4)) * 100,
-    //   z: 1,
-    // };
-};
-console.log(randomCoordinate());
-console.log(randomCoordinate());
-console.log(randomCoordinate());
-console.log(randomCoordinate());
-console.log(randomCoordinate());
-console.log(randomCoordinate());
-console.log(randomCoordinate());
-console.log(randomCoordinate());
-console.log(randomCoordinate());
-console.log(randomCoordinate());
-console.log(randomCoordinate());
-function doublePonit(point) {
-    return { x: point.x * 2, y: point.y * 2 };
+function calculatePayout(song) {
+    return song.numStreams * 0.33;
 }
-console.log(`doublePoint: ${doublePonit({ x: 2, y: 4 })}`);
-let a = 2;
+function printSong(song) {
+    console.log(`${song.title} - ${song.artist}`);
+}
+const mySong = {
+    title: 'UnchainedMelogy',
+    artist: 'Righteous Brothers',
+    numStreams: 12873321,
+    credits: {
+        producer: 'PhilSpector',
+        writer: 'Alex North',
+    },
+};
+console.log(`calculatePayout: ${calculatePayout(mySong)}`);
+printSong(mySong);
--- Objects/objects.ts
+++ Objects/objects.ts
@@ -15,37 +15,68 @@
 
 // let coordinate: { x: number; y: number } = { x: 34, y: 100 };
 // Creating Type Aliases
-type Point = {
-  x: number;
-  y: number;
+// type Point = {
+//   x: number;
+//   y: number;
+// };
+
+// const randomCoordinate = (): Point => {
+//   const coordinate = { x: 1, y: 1, z: 1 };
+//   return coordinate;
+//   // return {
+//   //   x: Number(Math.random().toFixed(4)) * 100,
+//   //   y: Number(Math.random().toFixed(4)) * 100,
+//   //   z: 1,
+//   // };
+// };
+// console.log(randomCoordinate());
+// console.log(randomCoordinate());
+// console.log(randomCoordinate());
+// console.log(randomCoordinate());
+// console.log(randomCoordinate());
+// console.log(randomCoordinate());
+// console.log(randomCoordinate());
+// console.log(randomCoordinate());
+// console.log(randomCoordinate());
+// console.log(randomCoordinate());
+// console.log(randomCoordinate());
+
+// function doublePonit(point: Point): Point {
+//   return { x: point.x * 2, y: point.y * 2 };
+// }
+
+// console.log(`doublePoint: ${doublePonit({ x: 2, y: 4 })}`);
+
+// type MyNum = number;
+// let a: MyNum = 2;
+
+type Song = {
+  title: string;
+  artist: string;
+  numStreams: number;
+  credits: {
+    producer: string;
+    writer: string;
+  };
 };
 
-const randomCoordinate = (): Point => {
-  const coordinate = { x: 1, y: 1, z: 1 };
-  return coordinate;
-  // return {
-  //   x: Number(Math.random().toFixed(4)) * 100,
-  //   y: Number(Math.random().toFixed(4)) * 100,
-  //   z: 1,
-  // };
-};
-console.log(randomCoordinate());
-console.log(randomCoordinate());
-console.log(randomCoordinate());
-console.log(randomCoordinate());
-console.log(randomCoordinate());
-console.log(randomCoordinate());
-console.log(randomCoordinate());
-console.log(randomCoordinate());
-console.log(randomCoordinate());
-console.log(randomCoordinate());
-console.log(randomCoordinate());
-
-function doublePonit(point: Point): Point {
-  return { x: point.x * 2, y: point.y * 2 };
+function calculatePayout(song: Song): number {
+  return song.numStreams * 0.33;
 }
 
-console.log(`doublePoint: ${doublePonit({ x: 2, y: 4 })}`);
+function printSong(song: Song): void {
+  console.log(`${song.title} - ${song.artist}`);
+}
 
-type MyNum = number;
-let a: MyNum = 2;
+const mySong: Song = {
+  title: 'UnchainedMelogy',
+  artist: 'Righteous Brothers',
+  numStreams: 12873321,
+  credits: {
+    producer: 'PhilSpector',
+    writer: 'Alex North',
+  },
+};
+
+console.log(`calculatePayout: ${calculatePayout(mySong)}`);
+printSong(mySong);
