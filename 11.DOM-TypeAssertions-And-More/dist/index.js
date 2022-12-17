b3bf6398480b2775bcd7b2dc5a39299ef4ed3711
Phuong Nguyen <phuongsover2@gmail.com>
2022-12-17 17:05:49 +0700

    Working with events

--- 11.DOM-TypeAssertions-And-More/dist/index.js
+++ 11.DOM-TypeAssertions-And-More/dist/index.js
@@ -1,7 +1,13 @@
 "use strict";
 const btn = document.getElementById('btn');
 const input = document.getElementById('todoInput');
+const form = document.querySelector('#todoForm');
 btn === null || btn === void 0 ? void 0 : btn.addEventListener('click', () => {
     input.value = 'Learn Japanese';
     alert(input.value);
 });
+const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('SUBMITTED!');
+};
+form.addEventListener('submit', handleSubmit);
--- 11.DOM-TypeAssertions-And-More/index.ts
+++ 11.DOM-TypeAssertions-And-More/index.ts
@@ -1,7 +1,14 @@
 const btn = document.getElementById('btn');
 const input = document.getElementById('todoInput')! as HTMLInputElement;
+const form = document.querySelector('#todoForm') as HTMLFormElement;
 
 btn?.addEventListener('click', () => {
   input.value = 'Learn Japanese';
   alert(input.value);
 });
+
+const handleSubmit = (e: SubmitEvent) => {
+  e.preventDefault();
+  console.log('SUBMITTED!');
+};
+form.addEventListener('submit', handleSubmit);
