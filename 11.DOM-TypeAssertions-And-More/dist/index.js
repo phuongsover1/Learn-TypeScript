306ad1fc9dd53cb1bdc6a39a0a0171336d9fd07d
Phuong Nguyen <phuongsover2@gmail.com>
2022-12-17 17:20:38 +0700

    Building a todolist

--- 11.DOM-TypeAssertions-And-More/dist/index.js
+++ 11.DOM-TypeAssertions-And-More/dist/index.js
@@ -2,12 +2,17 @@
 const btn = document.getElementById('btn');
 const input = document.getElementById('todoInput');
 const form = document.querySelector('#todoForm');
-btn === null || btn === void 0 ? void 0 : btn.addEventListener('click', () => {
-    input.value = 'Learn Japanese';
-    alert(input.value);
-});
+const list = document.querySelector('#todolist');
 const handleSubmit = (e) => {
     e.preventDefault();
     console.log('SUBMITTED!');
+    const newTodoText = input.value;
+    const newLI = document.createElement('LI');
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    newLI.append(newTodoText);
+    newLI.append(checkbox);
+    list.append(newLI);
+    input.value = '';
 };
 form.addEventListener('submit', handleSubmit);
--- 11.DOM-TypeAssertions-And-More/index.ts
+++ 11.DOM-TypeAssertions-And-More/index.ts
@@ -1,14 +1,20 @@
 const btn = document.getElementById('btn');
 const input = document.getElementById('todoInput')! as HTMLInputElement;
 const form = document.querySelector('#todoForm') as HTMLFormElement;
-
-btn?.addEventListener('click', () => {
-  input.value = 'Learn Japanese';
-  alert(input.value);
-});
+const list = document.querySelector('#todolist') as HTMLUListElement;
 
 const handleSubmit = (e: SubmitEvent) => {
   e.preventDefault();
   console.log('SUBMITTED!');
+
+  const newTodoText = input.value;
+  const newLI = document.createElement('LI');
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  newLI.append(newTodoText);
+  newLI.append(checkbox);
+
+  list.append(newLI);
+  input.value = '';
 };
 form.addEventListener('submit', handleSubmit);
