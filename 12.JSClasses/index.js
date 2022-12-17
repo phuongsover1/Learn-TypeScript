b784f09cd8e50524f0cb039a9c3d3182925c2eb7
Phuong Nguyen <phuongsover2@gmail.com>
2022-12-17 19:21:10 +0700

    Fields in class

--- 12.JSClasses/index.js
+++ 12.JSClasses/index.js
@@ -1,11 +1,23 @@
 'use strict';
 
 class Player {
+  score = 0;
+  numLives = 10;
+  constructor(first, last) {
+    this.first = first;
+    this.last = last;
+  }
   taunt() {
     console.log('BOOYAH!');
   }
+  loseLife() {
+    this.numLives -= 1;
+  }
 }
 
-const player1 = new Player();
+const player1 = new Player('phuong', 'nguyen');
 player1.taunt();
 console.log(player1);
+
+const player2 = new Player('Khang', 'nguyen');
+console.log(player2);
