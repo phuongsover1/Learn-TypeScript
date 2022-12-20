0cf8e0e61ef6319d9fa4501b6f6d974ed0057e01
Phuong Nguyen <phuongsover2@gmail.com>
2022-12-20 23:59:58 +0700

    Typeof Guards

---
+++ 15.Type-narrowing/dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+function triple(value) {
+    if (typeof value === "string")
+        return value.repeat(3);
+    return value * 3;
+}
---
+++ 15.Type-narrowing/index.ts
@@ -0,0 +1,4 @@
+function triple(value: number | string) {
+  if (typeof value === "string") return value.repeat(3);
+  return value * 3;
+}
