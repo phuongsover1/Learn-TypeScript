599b66e506bc660e60c721ee0c581dbd88d0f86d
Phuong Nguyen <phuongsover2@gmail.com>
2022-12-21 01:03:41 +0700

    Instanceof Narrowing

--- 15.Type-narrowing/index.ts
+++ 15.Type-narrowing/index.ts
@@ -17,3 +17,51 @@ function someFunc(x: string | number, y: string | boolean): void {
     y.toUpperCase();
   }
 }
+
+// 'IN' NARROWING
+interface Movie {
+  title: string;
+  duration: number;
+}
+
+interface TVShow {
+  title: string;
+  numEpisodes: number;
+  episodeDuration: number;
+}
+
+function getRuntime(media: Movie | TVShow): number {
+  if ("numEpisodes" in media) {
+    return media.numEpisodes * media.episodeDuration; // TS convert force 'media' to 'TVShow' automatically
+  }
+  return media.duration;
+}
+
+// instanceof narrowing
+function printFullDate(date: string | Date): void {
+  if (date instanceof Date) {
+    console.log(date.toUTCString());
+  } else {
+    console.log(new Date(date).toUTCString());
+  }
+}
+
+class User {
+  constructor(public username: string) {}
+}
+
+class Company {
+  constructor(public name: string) {}
+}
+
+function printName(entity: User | Company) {
+  // use one of two approaches below
+  // if ('username' in entity) {
+  //   entity;
+  // }
+  if (entity instanceof User) {
+    entity;
+  } else {
+    entity;
+  }
+}
