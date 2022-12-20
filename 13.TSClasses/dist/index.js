965b171ea992bc1e9fe06fa442540da0e49376fb
Phuong Nguyen <phuongsover2@gmail.com>
2022-12-20 18:28:21 +0700

    Classes declaration in TS

---
+++ 13.TSClasses/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+class Player {
+    constructor(first, last) {
+        this.first = first;
+        this.last = last;
+    }
+}
+const obj = new Player("phuong", "nguyen");
---
+++ 13.TSClasses/index.ts
@@ -0,0 +1,10 @@
+class Player {
+  private first: string;
+  private last: string;
+  constructor(first: string, last: string) {
+    this.first = first;
+    this.last = last;
+  }
+}
+
+const obj: Player = new Player("phuong", "nguyen");
