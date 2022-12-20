1058b8215d00a5fefbea6c4441f3703d3bd91ab4
Phuong Nguyen <phuongsover2@gmail.com>
2022-12-20 21:58:08 +0700

    Built-in generics dom functions

---
+++ 14.Generics/dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+const nums = [];
+const colors = [];
+const inputEl = document.querySelector("#username");
+console.dir(inputEl);
+if (inputEl !== null)
+    inputEl.value = "Hacked!!!";
+const btn = document.querySelector(".btn");
+if (btn !== null) {
+}
---
+++ 14.Generics/index.ts
@@ -0,0 +1,11 @@
+const nums: Array<number> = [];
+const colors: Array<string> = [];
+
+const inputEl = document.querySelector<HTMLInputElement>("#username");
+console.dir(inputEl);
+if (inputEl !== null) inputEl.value = "Hacked!!!";
+
+const btn = document.querySelector<HTMLButtonElement>(".btn");
+
+if (btn !== null) {
+}
