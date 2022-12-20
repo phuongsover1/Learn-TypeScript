648278f9a5a65c29e311a4a5e6f491c80e6e55de
Phuong Nguyen <phuongsover2@gmail.com>
2022-12-20 19:15:14 +0700

    Getter and setter in TS

--- 13.TSClasses/dist/index.js
+++ 13.TSClasses/dist/index.js
@@ -1,9 +1,9 @@
 "use strict";
 class Player {
-    constructor(first, last) {
+    constructor(first, last, _score) {
         this.first = first;
         this.last = last;
-        this.score = 0;
+        this._score = _score;
         this.description = "Player desription";
     }
     get getFirst() {
@@ -12,7 +12,18 @@ class Player {
     get getLast() {
         return this.last;
     }
+    get fullName() {
+        return `${this.first} ${this.last}`;
+    }
+    get score() {
+        return this._score;
+    }
+    set score(newScore) {
+        if (newScore < 0)
+            throw new Error("Score must be greater than 0");
+        this._score = newScore;
+    }
 }
-const obj = new Player("phuong", "nguyen");
-obj.description = "Hello";
-console.log(`first: ${obj.getFirst}, last: ${obj.getLast}`);
+const obj = new Player("phuong", "nguyen", 12);
+console.log(obj.score);
+obj.score = -4545;
--- 13.TSClasses/index.ts
+++ 13.TSClasses/index.ts
@@ -2,10 +2,14 @@ class Player {
   // private first: string;
   // private last: string;
 
-  readonly score: number = 0;
+  // readonly score: number = 0;
 
   public description: string = "Player desription";
-  constructor(private first: string, private last: string) {}
+  constructor(
+    private first: string,
+    private last: string,
+    private _score: number
+  ) {}
 
   get getFirst() {
     return this.first;
@@ -14,11 +18,21 @@ class Player {
   get getLast() {
     return this.last;
   }
+
+  get fullName(): string {
+    return `${this.first} ${this.last}`;
+  }
+
+  get score() {
+    return this._score;
+  }
+
+  set score(newScore: number) {
+    if (newScore < 0) throw new Error("Score must be greater than 0");
+    this._score = newScore;
+  }
 }
 
-const obj: Player = new Player("phuong", "nguyen");
-// obj.score = 3; // error because score is read-only
-//
-obj.description = "Hello";
-
-console.log(`first: ${obj.getFirst}, last: ${obj.getLast}`);
+const obj: Player = new Player("phuong", "nguyen", 12);
+console.log(obj.score);
+obj.score = -4545; // error
