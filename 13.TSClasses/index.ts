961d7cedcfe86450ee3ee556b62182e8ae49ed20
Phuong Nguyen <phuongsover2@gmail.com>
2022-12-20 18:34:59 +0700

    Readonly properties TS

--- 13.TSClasses/index.ts
+++ 13.TSClasses/index.ts
@@ -1,6 +1,8 @@
 class Player {
   private first: string;
   private last: string;
+
+  readonly score: number = 0;
   constructor(first: string, last: string) {
     this.first = first;
     this.last = last;
@@ -8,3 +10,4 @@ class Player {
 }
 
 const obj: Player = new Player("phuong", "nguyen");
+// obj.score = 3; // error because score is read-only
