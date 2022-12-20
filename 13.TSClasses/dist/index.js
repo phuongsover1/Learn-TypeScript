b4f2928e2d7b186a47e184c2081d82769d881ebd
Phuong Nguyen <phuongsover2@gmail.com>
2022-12-20 18:45:41 +0700

    Public keyword in TS

--- 13.TSClasses/dist/index.js
+++ 13.TSClasses/dist/index.js
@@ -2,8 +2,10 @@
 class Player {
     constructor(first, last) {
         this.score = 0;
+        this.description = "Player desription";
         this.first = first;
         this.last = last;
     }
 }
 const obj = new Player("phuong", "nguyen");
+obj.description = "Hello";
--- 13.TSClasses/index.ts
+++ 13.TSClasses/index.ts
@@ -3,6 +3,9 @@ class Player {
   private last: string;
 
   readonly score: number = 0;
+
+  public description: string = "Player desription";
+
   constructor(first: string, last: string) {
     this.first = first;
     this.last = last;
@@ -11,3 +14,5 @@ class Player {
 
 const obj: Player = new Player("phuong", "nguyen");
 // obj.score = 3; // error because score is read-only
+//
+obj.description = "Hello";
