68e56f3a479f1cc419f107d60cf01d4ae694ed8d
Phuong Nguyen <phuongsover2@gmail.com>
2022-12-21 00:12:58 +0700

    Truthiness guards

--- 15.Type-narrowing/index.ts
+++ 15.Type-narrowing/index.ts
@@ -2,3 +2,10 @@ function triple(value: number | string) {
   if (typeof value === "string") return value.repeat(3);
   return value * 3;
 }
+
+const el = document.getElementById("idk");
+if (el) {
+  el;
+} else {
+  el;
+}
